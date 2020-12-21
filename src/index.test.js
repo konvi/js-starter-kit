d99202a6cddb33ce2f30257c43e1ba20ff74611e
konvi <gelyako@rambler.ru>
2020-12-21 10:37:56 -0800

    First HTTP Call

--- src/index.test.js
+++ src/index.test.js
@@ -8,13 +8,13 @@ describe('Our first test', () => {
     });
 });
 
-describe('index.html', () => {
+describe('should have h1 Users', () => {
     // TODO: what is done?
     it('should say hello', (done) => {
         const index = fs.readFileSync('./src/index.html', 'utf-8');
         jsdom.env(index, function (err, window) {
             const h1 = window.document.getElementsByTagName('h1')[0];
-            expect(h1.innerHTML).to.equal("Hello World?");
+            expect(h1.innerHTML).to.equal("Users");
             done();
             window.close();
         });
